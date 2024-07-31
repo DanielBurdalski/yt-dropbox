24e4b6f7193699edbca94a9b61695aa3bf9c98ae
DanielBurdalski <45230866+DanielBurdalski@users.noreply.github.com>
2024-07-31 21:42:14 +0200

    Update archive_script.py

--- archive_script.py
+++ archive_script.py
@@ -3,7 +3,6 @@ from datetime import datetime
 import os
 import sys
 import dropbox
-import subprocess
 
 DROPBOX_ACCESS_TOKEN = os.getenv('DROPBOX_ACCESS_TOKEN')
 
@@ -47,34 +46,4 @@ def archive_last_live():
         return
 
     ydl_opts = {
-        'format': 'best/bestvideo+bestaudio/best',
-        'outtmpl': '%(title)s-%(id)s.%(ext)s',
-        'merge_output_format': 'mp4'
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(second_live_url, download=True)
-        upload_date = datetime.strptime(info['upload_date'], '%Y%m%d').strftime('%d_%m_%Y')
-        filename = f"PaszaTV-{upload_date}.{info['ext']}"
-        os.rename(ydl.prepare_filename(info), filename)
-
-    try:
-        dropbox_path = f"/{filename}"
-        upload_to_dropbox(filename, dropbox_path)
-    except Exception as e:
-        print(f"Error during upload: {e}")
-    finally:
-        os.remove(filename)
-
-if __name__ == "__main__":
-    try:
-        # Update yt-dlp to the latest version
-        subprocess.run(['pip', 'install', '--upgrade', 'yt-dlp'])
-        
-        # Install ffmpeg
-        subprocess.run(['apt-get', 'update'])
-        subprocess.run(['apt-get', 'install', '-y', 'ffmpeg'])
-        
-        archive_last_live()
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        sys.exit(1)
+        'format
