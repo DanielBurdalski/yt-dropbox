770b690afc71454e5de0d764fde2e4258f2f1d8a
DanielBurdalski <45230866+DanielBurdalski@users.noreply.github.com>
2024-08-01 09:11:53 +0200

    Update download_stream.py

--- download_stream.py
+++ download_stream.py
@@ -8,9 +8,10 @@ CHANNEL_URL = 'https://www.youtube.com/@MajsterProjekt/streams'
 def get_last_stream():
     # Konfiguracja yt-dlp
     ydl_opts = {
-        'format': 'bestvideo[height<=720][ext=mp4]+bestaudio[ext=m4a]/best[height<=720][ext=mp4]/best',
+        'format': 'bestvideo[height<=720]+bestaudio/best[height<=720]/best',
         'outtmpl': '%(title)s-%(id)s.%(ext)s',
-        'daterange': datetime.now() - timedelta(days=365),  # Nie starsze niż rok
+        'daterange': f"today-365day",  # Nie starsze niż rok
+        'playlistend': 1,  # Pobierz tylko najnowszy stream
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -18,13 +19,14 @@ def get_last_stream():
             # Pobierz informacje o streamach
             info = ydl.extract_info(CHANNEL_URL, download=False)
             
-            # Wybierz ostatni zakończony stream
-            for entry in info['entries']:
-                if entry['live_status'] == 'was_live':
-                    print(f"Znaleziono ostatni stream: {entry['title']}")
-                    # Pobierz stream
-                    ydl.download([entry['webpage_url']])
-                    return f"{entry['title']}-{entry['id']}.mp4"
+            if 'entries' in info:
+                # Wybierz ostatni zakończony stream
+                for entry in info['entries']:
+                    if entry.get('live_status') == 'was_live':
+                        print(f"Znaleziono ostatni stream: {entry['title']}")
+                        # Pobierz stream
+                        ydl.download([entry['webpage_url']])
+                        return f"{entry['title']}-{entry['id']}.{entry['ext']}"
             
             print("Nie znaleziono odpowiedniego streamu.")
             return None
