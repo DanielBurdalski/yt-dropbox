19e73d12a74bfccd83c72bfc98ef3e32c74300f3
DanielBurdalski <45230866+DanielBurdalski@users.noreply.github.com>
2024-08-01 13:40:48 +0200

    Update download_stream.py

--- download_stream.py
+++ download_stream.py
@@ -1,29 +1,9 @@
-import os
-import sys
 import yt_dlp
-from doodstream import DoodStream
+import os
+from datetime import datetime, timedelta
 
 # URL kanału
 CHANNEL_URL = 'https://www.youtube.com/@MajsterProjekt/streams'
-DOODSTREAM_API_KEY = os.getenv('DOODSTREAM_API_KEY')
-
-if not DOODSTREAM_API_KEY:
-    print("Brak klucza API DOODSTREAM. Ustaw zmienną środowiskową DOODSTREAM_API_KEY.")
-    sys.exit(1)
-
-d = DoodStream(DOODSTREAM_API_KEY)
-
-def is_file_uploaded(filename):
-    try:
-        response = d.my_uploads()
-        if 'result' in response and isinstance(response['result'], list):
-            for file in response['result']:
-                if filename in file['title']:
-                    print(f"Plik '{filename}' jest już przesłany.")
-                    return True
-    except Exception as e:
-        print(f"Błąd podczas sprawdzania plików na DoodStream: {e}")
-    return False
 
 def get_last_stream():
     # Konfiguracja yt-dlp
@@ -42,14 +22,10 @@ def get_last_stream():
                 # Wybierz ostatni zakończony stream
                 for entry in info['entries']:
                     if entry.get('live_status') == 'was_live':
-                        filename = f"{entry['title']}-{entry['id']}"
-                        if is_file_uploaded(filename):
-                            print(f"Plik '{filename}' jest już przesłany. Pobieranie anulowane.")
-                            return None
                         print(f"Znaleziono ostatni stream: {entry['title']}")
                         # Pobierz stream
                         ydl.download([entry['webpage_url']])
-                        return f"{filename}.{entry['ext']}"
+                        return f"{entry['title']}-{entry['id']}.{entry['ext']}"
             
             print("Nie znaleziono odpowiedniego streamu.")
             return None
