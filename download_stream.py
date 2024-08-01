b6f638f1ed90df7d3fbb99f88ef136f19de01861
DanielBurdalski <45230866+DanielBurdalski@users.noreply.github.com>
2024-08-01 14:03:22 +0200

    Update download_stream.py

--- download_stream.py
+++ download_stream.py
@@ -4,20 +4,6 @@ from datetime import datetime, timedelta
 
 # URL kanału
 CHANNEL_URL = 'https://www.youtube.com/@MajsterProjekt/streams'
-UPLOADS_FILE = 'uploads.txt'
-
-def has_been_downloaded(file_name):
-    """Sprawdza, czy plik o podanej nazwie został już pobrany."""
-    if not os.path.exists(UPLOADS_FILE):
-        return False
-    with open(UPLOADS_FILE, 'r') as f:
-        downloaded_files = f.read().splitlines()
-    return file_name in downloaded_files
-
-def mark_as_downloaded(file_name):
-    """Zapisuje nazwę pobranego pliku do uploads.txt."""
-    with open(UPLOADS_FILE, 'a') as f:
-        f.write(file_name + '\n')
 
 def get_last_stream():
     # Konfiguracja yt-dlp
@@ -36,21 +22,10 @@ def get_last_stream():
                 # Wybierz ostatni zakończony stream
                 for entry in info['entries']:
                     if entry.get('live_status') == 'was_live':
-                        file_name = f"{entry['title']}-{entry['id']}.{entry['ext']}"
-                        
-                        # Sprawdź, czy plik został już pobrany
-                        if has_been_downloaded(file_name):
-                            print(f"Plik '{file_name}' został już pobrany.")
-                            return None
-                        
                         print(f"Znaleziono ostatni stream: {entry['title']}")
                         # Pobierz stream
                         ydl.download([entry['webpage_url']])
-                        
-                        # Zapisz nazwę pobranego pliku
-                        mark_as_downloaded(file_name)
-                        
-                        return file_name
+                        return f"{entry['title']}-{entry['id']}.{entry['ext']}"
             
             print("Nie znaleziono odpowiedniego streamu.")
             return None
