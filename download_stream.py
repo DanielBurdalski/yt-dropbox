24934510771e28ced011c321dab5651dbd806422
DanielBurdalski <45230866+DanielBurdalski@users.noreply.github.com>
2024-08-02 14:52:08 +0200

    Update download_stream.py

--- download_stream.py
+++ download_stream.py
@@ -1,35 +1,11 @@
 import yt_dlp
 import os
 import json
-import time
 import subprocess
-from datetime import datetime, timedelta
+from datetime import datetime
 
 # URL kanału
-CHANNEL_URL = 'https://www.youtube.com/@onet/streams'
-
-def check_if_recorded(file_name):
-    record_info_file = 'record_info.json'
-    if not os.path.exists(record_info_file):
-        return False
-    with open(record_info_file, 'r') as f:
-        record_info = json.load(f)
-    if file_name in record_info:
-        last_record_time = datetime.fromisoformat(record_info[file_name])
-        if datetime.now() - last_record_time < timedelta(days=7):
-            return True
-    return False
-
-def update_record_info(file_name):
-    record_info_file = 'record_info.json'
-    if os.path.exists(record_info_file):
-        with open(record_info_file, 'r') as f:
-            record_info = json.load(f)
-    else:
-        record_info = {}
-    record_info[file_name] = datetime.now().isoformat()
-    with open(record_info_file, 'w') as f:
-        json.dump(record_info, f)
+CHANNEL_URL = 'https://www.youtube.com/@PaszaTV/streams'
 
 def get_channel_name(channel_url):
     ydl_opts = {
@@ -50,30 +26,25 @@ def record_live_stream(video_url):
         try:
             info = ydl.extract_info(video_url, download=False)
             channel_name = get_channel_name(CHANNEL_URL)
-            file_name = f"{channel_name}-{datetime.now().strftime('%Y-%m-%d')}.mp4"
+            file_name = f"{channel_name}-{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.mp4"
             
-            if not check_if_recorded(file_name):
-                print(f"Rozpoczęto nagrywanie streamu: {info['title']}")
-                
-                # Pobierz URL streamu
-                stream_url = info['formats'][-1]['url']
-                
-                # Użyj FFmpeg do nagrania streamu
-                ffmpeg_command = [
-                    'ffmpeg',
-                    '-i', stream_url,
-                    '-c', 'copy',
-                    '-t', '20',  # Limit nagrywania do 3 godzin (10800 sekund)
-                    file_name
-                ]
-                
-                subprocess.run(ffmpeg_command)
-                
-                update_record_info(file_name)
-                return file_name
-            else:
-                print(f"Stream {info['title']} był już nagrywany w ciągu ostatnich 7 dni. Pomijam.")
-                return None
+            print(f"Rozpoczęto nagrywanie streamu: {info['title']}")
+            
+            # Pobierz URL streamu
+            stream_url = info['formats'][-1]['url']
+            
+            # Użyj FFmpeg do nagrania streamu
+            ffmpeg_command = [
+                'ffmpeg',
+                '-i', stream_url,
+                '-c', 'copy',
+                '-t', '20',  # Limit nagrywania do 20 sekund
+                file_name
+            ]
+            
+            subprocess.run(ffmpeg_command)
+            
+            return file_name
         except Exception as e:
             print(f"Wystąpił błąd podczas nagrywania: {e}")
             return None
@@ -97,19 +68,15 @@ def check_for_live_streams():
             return None
 
 if __name__ == "__main__":
-    while True:
-        print("Sprawdzanie aktywnych streamów...")
-        live_stream_url = check_for_live_streams()
-        
-        if live_stream_url:
-            print(f"Znaleziono aktywny stream: {live_stream_url}")
-            recorded_file = record_live_stream(live_stream_url)
-            if recorded_file:
-                print(f"Pomyślnie nagrano plik: {recorded_file}")
-            else:
-                print("Nie udało się nagrać streamu lub stream był już nagrywany.")
+    print("Sprawdzanie aktywnych streamów...")
+    live_stream_url = check_for_live_streams()
+    
+    if live_stream_url:
+        print(f"Znaleziono aktywny stream: {live_stream_url}")
+        recorded_file = record_live_stream(live_stream_url)
+        if recorded_file:
+            print(f"Pomyślnie nagrano plik: {recorded_file}")
         else:
-            print("Nie znaleziono aktywnych streamów.")
-        
-        print("Oczekiwanie 5 minut przed kolejnym sprawdzeniem...")
-        time.sleep(300)  # Czekaj 5 minut przed kolejnym sprawdzeniem
+            print("Nie udało się nagrać streamu.")
+    else:
+        print("Nie znaleziono aktywnych streamów.")
