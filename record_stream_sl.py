fe5bb4fca766b167aceb511c0158dca439039045
DanielBurdalski <45230866+DanielBurdalski@users.noreply.github.com>
2024-08-03 11:35:24 +0200

    Update record_stream_sl.py

--- record_stream_sl.py
+++ record_stream_sl.py
@@ -8,7 +8,7 @@ import requests
 import re
 
 # URL kanału
-CHANNEL_URL = 'https://www.youtube.com/@Telewizja_Republika/streams'
+CHANNEL_URL = 'https://www.youtube.com/@ProboszczProboszcz/streams'
 
 def print_message(message):
     print(message, flush=True)
