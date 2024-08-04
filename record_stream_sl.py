eb564f8b54252843b13bca92e94890239212e4dc
DanielBurdalski <45230866+DanielBurdalski@users.noreply.github.com>
2024-08-04 19:31:11 +0200

    Update record_stream_sl.py

--- record_stream_sl.py
+++ record_stream_sl.py
@@ -8,7 +8,7 @@ import requests
 import re
 
 # URL kanału
-CHANNEL_URL = os.getenv('https://www.youtube.com/@bystrzaktv/streams')
+CHANNEL_URL = 'https://www.youtube.com/@bystrzaktv/streams'
 
 def print_message(message):
     print(message, flush=True)
