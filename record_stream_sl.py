c6d8e52a28d81dba15395155a6eb3bbb40199ab8
DanielBurdalski <45230866+DanielBurdalski@users.noreply.github.com>
2024-08-08 21:45:53 +0200

    Update record_stream_sl.py

--- record_stream_sl.py
+++ record_stream_sl.py
@@ -6,9 +6,17 @@ import sys
 import time
 import requests
 import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
 # URL kanału
 CHANNEL_URL = 'https://www.youtube.com/@PaszaTV/streams'
+
+# Dane logowania do YouTube
+YOUTUBE_EMAIL = os.getenv('YOUTUBE_EMAIL')
+YOUTUBE_PASSWORD = os.getenv('YOUTUBE_PASSWORD')
 
 def print_message(message):
     print(message, flush=True)
@@ -27,7 +35,7 @@ def record_live_stream(video_url):
     try:
         channel_name = get_channel_name(CHANNEL_URL) or "unknown"
         file_name = f"{channel_name}-{datetime.now().strftime('%d-%m-%Y_%H-%M')}.mp4"
-        
+
         streamlink_command = [
             'streamlink',
             '--stream-segment-threads', '2',
@@ -37,16 +45,16 @@ def record_live_stream(video_url):
             '-o', file_name,
             video_url, 'best'
         ]
-        
+
         print_message(f"Rozpoczęcie nagrywania: {' '.join(streamlink_command)}")
         process = subprocess.Popen(streamlink_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         time.sleep(19000)  # Nagrywaj przez 19000s
         process.terminate()
-        
+
         stdout, stderr = process.communicate()
         print_message(f"Streamlink stdout: {stdout.decode()}")
         print_message(f"Streamlink stderr: {stderr.decode()}")
-        
+
         if os.path.exists(file_name):
             print_message(f"Plik został pomyślnie utworzony: {file_name}")
             return file_name
@@ -66,7 +74,7 @@ def check_for_live_streams():
             video_id = match.group(1)
             live_url = f"https://www.youtube.com/watch?v={video_id}"
             print_message(f"Znaleziono potencjalny aktywny stream: {live_url}")
-            
+
             # Sprawdź, czy stream jest rzeczywiście aktywny
             streams = streamlink.streams(live_url)
             if streams:
@@ -81,15 +89,60 @@ def check_for_live_streams():
         print_message(f"Wystąpił błąd podczas sprawdzania streamów: {e}")
         return None
 
+def login_to_youtube():
+    try:
+        # Konfiguracja opcji dla przeglądarki Chrome w trybie headless
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+
+        # Inicjalizacja przeglądarki
+        driver = webdriver.Chrome(options=chrome_options)
+
+        # Otwórz stronę YouTube
+        driver.get("https://www.youtube.com")
+
+        # Znajdź i kliknij przycisk "Zaloguj się"
+        login_button = driver.find_element(By.XPATH, '//*[@id="buttons"]/ytd-button-renderer/a')
+        login_button.click()
+
+        # Znajdź pole tekstowe dla adresu e-mail i wprowadź adres e-mail
+        email_field = driver.find_element(By.ID, 'identifierId')
+        email_field.send_keys(YOUTUBE_EMAIL)
+        email_field.send_keys(Keys.RETURN)
+
+        # Poczekaj, aż strona się załaduje
+        time.sleep(2)
+
+        # Znajdź pole tekstowe dla hasła i wprowadź hasło
+        password_field = driver.find_element(By.NAME, 'password')
+        password_field.send_keys(YOUTUBE_PASSWORD)
+        password_field.send_keys(Keys.RETURN)
+
+        # Poczekaj, aż strona się załaduje
+        time.sleep(5)
+
+        # Zamknij przeglądarkę po zakończeniu
+        driver.quit()
+
+        print_message("Pomyślnie zalogowano do YouTube")
+    except Exception as e:
+        print_message(f"Błąd podczas logowania do YouTube: {e}")
+
 if __name__ == "__main__":
     print_message("Rozpoczęcie działania skryptu")
-    
+
+    # Zaloguj się do YouTube
+    login_to_youtube()
+
     try:
         live_stream_url = check_for_live_streams()
     except Exception as e:
         print_message(f"Nieoczekiwany błąd podczas sprawdzania streamów: {e}")
         sys.exit(1)
-    
+
     if live_stream_url:
         print_message(f"Znaleziono aktywny stream: {live_stream_url}")
         recorded_file = record_live_stream(live_stream_url)
@@ -99,5 +152,5 @@ if __name__ == "__main__":
             print_message("Nie udało się nagrać streamu.")
     else:
         print_message("Nie znaleziono aktywnych streamów.")
-    
+
     print_message("Zakończenie działania skryptu")
